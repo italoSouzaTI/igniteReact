be2ea02be13c04a87ac3f79fb906e711d78d258f
Italo Araujo Souza <italoasouzat.i@gmail.com>
2021-03-10 17:53:57 -0300

    feat: resolvendo bug do webpack

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,3 @@
-import React from 'react';
-import {App}from './App'
+import React, { render } from 'react-dom';
+import { App } from './App'
+render(<h1>Test</h1>, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
+
 const path = require('path') // Orquestrador dos caminhos dos arquivos
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 module.exports = {
-    mode: isDevelopment ? 'development' : 'production',
-    entry: path.resolve(__dirname, 'src', 'index.jsx'), //O arquivo principal da aplicação
-    output: {
-        path: path.resolve(__dirname, 'dist'),// Caminho de saida dos arquivos convertidos
-        filename: 'bundle.js'
-    },
-    //Quais formatos nossa aplicação pode ler
-    resolve: {
-        extensions: ['.js', '.jsx'],
+  mode: isDevelopment ? 'development' : 'production',
+  entry: path.resolve(__dirname, 'src', 'index.jsx'), //O arquivo principal da aplicação
+  output: {
+    path: path.resolve(__dirname, 'dist'),// Caminho de saida dos arquivos convertidos
+    filename: 'bundle.js'
+  },
+  //Quais formatos nossa aplicação pode ler
+  resolve: {
+    extensions: ['.js', '.jsx'],
 
-    },
-    // lidando com cada tipo de arquivo
-    module: {
-        rules: [
-            {
-                test:/\.jsx$/,
-                exclude:/node_modules/,
-                use: 'babel-loader',
-            }
-        ],
-    }
+  },
+  // lidando com cada tipo de arquivo
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      }
+    ],
+  }
 };
